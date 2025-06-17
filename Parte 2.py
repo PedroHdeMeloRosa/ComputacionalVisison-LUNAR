b957eb780a44fe8a71853e2d3c97afcabc60b9d3
Pedro H de Melo Rosa <phm.rosa@unesp.br>
2025-06-17 20:47:59 -0300

    ola

--- Parte 2.py
+++ Parte 2.py
@@ -1,15 +1,17 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("C:\\Users\Pedro\Desktop\\UNESP-DOCS\DOCS. PESSSUAIS\Instagram\IMG_20231029_233121_873.jpg", cv2.IMREAD_COLOR)
+cap = cv2.VideoCapture(0)
 
-cv2.line(img, (0,0) , (150,150), (255,255,255) , 10)
-            #Ponto inicial e final em x e y; cor do rabisco; espessura da linha
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame',frame)
+    cv2.imshow('gray',gray)
 
-cv2.rectangle(img, (0,0), (500,500), (255,0,255), (2) )
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-#cv2.circle, funciona do mesmo jeito
+cap.release()
 
-cv2.imshow("Imagem",img)
-cv2.waitKey(0)
 cv2.destroyAllWindows()
