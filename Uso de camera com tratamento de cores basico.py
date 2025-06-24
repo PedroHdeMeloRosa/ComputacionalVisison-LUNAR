dc374f86e09351cbbe2bbefb56b5d882fabde138
Pedro H de Melo Rosa <phm.rosa@unesp.br>
2025-06-24 14:50:51 -0300

    ola

--- Uso de camera com tratamento de cores basico.py
+++ Uso de camera com tratamento de cores basico.py
@@ -2,18 +2,6 @@ import cv2
 import numpy
 import matplotlib.pyplot as plt
 
-
-'''path = "C:/Users\Pedro\Pictures\Screenshots\Captura de tela 2025-03-26 161323.png"
-img = cv2.imread(path , cv2.IMREAD_GRAYSCALE)
-
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyWindow()
-
-plt.imshow(img, cmap='gray', interpolation= 'bicubic')
-plt.plot([50,500],[800,48], 'c' , linewidth=2)
-plt.show()'''
-
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
